c3877700290391b167c16661c06a285efd0a2811
Cyxapic <truecyxapic@yandex.ru>
2018-11-24 19:32:46 +0300

    CLEANUP

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ game = Game(screen)
 
 pygame.mixer.music.load(sound)
 pygame.mixer.music.set_volume(0.5)
-# pygame.mixer.music.play(-1)
+pygame.mixer.music.play(-1)
 
 clock = pygame.time.Clock()
 
